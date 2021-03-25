553e0ce1025043ce40e31d0fd8bec5d25f0f0126
Ville Leppänen <ville.leppanen@heeros.com>
2021-03-25 12:22:39 +0200

    works

--- lia-sdk-macos/sir-killalot/warrior_ai.py
+++ lia-sdk-macos/sir-killalot/warrior_ai.py
@@ -9,18 +9,12 @@ from lia.bot import Bot
 from lia.networking_client import connect
 
 
-class Tactic:
-    LINEAR = "LINEAR"
-    FLANK_RIGHT = "FLANK_RIGHT"
-
-
 YELL = ["DIE!", "VICTORY IS OURS!", "I AM YOUR FATHER!"]
 
 
 def spawn(state, api):
     if state["resources"] >= constants.WARRIOR_PRICE:
         api.spawn_unit(UnitType.WARRIOR)
-        state["units"][-1]["tactic"] = Tactic.FLANK_RIGHT
 
 
 def act(state, api, unit):
@@ -29,31 +23,11 @@ def act(state, api, unit):
 
 
 def move(state, api, unit):
-    # If the unit is not going anywhere, we send it
-    # to a random valid location on the map.
-    if len(unit["navigationPath"]) == 0:
-        api.say_something(unit["id"], random.choice(YELL))
-        # Generate new x and y until you get a position on the map
-        # where there is no obstacle.
-        if not "tactic" in unit:
-            unit["tactic"] = random.choice([Tactic.LINEAR, Tactic.FLANK_RIGHT])
-        if unit["tactic"] == Tactic.FLANK_RIGHT:
-            move_flank(api, unit)
-        elif unit["tactic"] == Tactic.LINEAR:
-            move_linear(api, unit)
-
-
-def move_flank(api, unit):
-    if get_starting_pos() == "BOTTOM":
-        if unit["x"] >= constants.MAP_WIDTH / 2:
-            api.navigation_stop(unit["id"])
-        else:
-            api.navigation_start(unit["id"], constants.MAP_WIDTH, unit["y"])
-    else:
-        if unit["x"] < constants.MAP_WIDTH / 2:
-            api.navigation_stop(unit["id"])
-        else:
-            api.navigation_start(unit["id"], 0, unit["y"])
+    unit["advancing"] = True
+    api.say_something(unit["id"], random.choice(YELL))
+    # Generate new x and y until you get a position on the map
+    # where there is no obstacle
+    move_linear(api, unit)
 
 
 def move_linear(api, unit):
